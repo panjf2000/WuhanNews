01040be67e62441550a690ce347ed6d64c9cdcbd
panjf2000 <panjf2000@gmail.com>
2015-05-08 22:26:23 +0800

    增加API接口常量

--- app/src/main/java/net/wutnews/app/app/util/uurl.java
+++ app/src/main/java/net/wutnews/app/app/util/uurl.java
@@ -10,14 +10,15 @@ public class uurl {
     public static final HttpSender.HttpMode MODE = HttpSender.HttpMode.Get;
     public static final String SERVER_URL = "Http://lgfb.wutnews.net/";//服务器地址
     public static final String IMG_URL = "http://lgfb.wutnews.net/data/upload/";//图片下载地址前缀
+    public static final String AVATAR_URL=SERVER_URL+"data/upload/avatar/";//头像URL前缀
     public static final String AES_KEY = "wutnews77AZXtvRf";//AES加密KEY
-
 
 
     public static final String GetNewsList = SERVER_URL+"api/post/info";//新闻列表
     public static final String GetTermList = SERVER_URL+"api/term/info";//栏目列表
     public static final String GetLogin = SERVER_URL+"api/login/school";//校园卡登录
     public static final String GetGuestLogin = SERVER_URL+"api/login/guest";//游客登录
+    public static final String GetUserInfo=SERVER_URL+"api/Userinfo/info";//用户信息
     public static final String CollectNews = SERVER_URL+"api/collect/newa";//收藏新闻
     public static final String CollectList = SERVER_URL+"api/collect/show";//获取收藏
     public static final String DeleteCollection=SERVER_URL+"api/collect/delete";//删除收藏
@@ -26,4 +27,6 @@ public class uurl {
     public static final String SendComment=SERVER_URL+"api/comment/insert";
     public static final String CommentList=SERVER_URL+"api/comment/show";
     public static final String DeleteComment=SERVER_URL+"api/comment/delete";
+    public static final String SetSignature=SERVER_URL+"api/Userinfo/signature";
+
 }
