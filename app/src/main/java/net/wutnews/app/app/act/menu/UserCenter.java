8957e7fb6afc8fe24667ba3e838707dfa7c4a74b
panjf2000 <panjf2000@gmail.com>
2015-05-21 00:06:12 +0800

    在个人中心增加获取用户信息和修改用户信息的功能

--- app/src/main/java/net/wutnews/app/app/act/menu/UserCenter.java
+++ app/src/main/java/net/wutnews/app/app/act/menu/UserCenter.java
@@ -5,31 +5,36 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import com.lidroid.xutils.DbUtils;
 import com.lidroid.xutils.util.LogUtils;
-
 import net.wutnews.app.R;
 import net.wutnews.app.app.act.Login.LoginAct;
 import net.wutnews.app.app.act.app.AppBaseAct;
 import net.wutnews.app.app.entiy.DBUserinfo;
+import net.wutnews.app.app.entiy.GetUserDe;
+import net.wutnews.app.app.entiy.GetUserInfo;
+import net.wutnews.app.app.entiy.SetUser;
 import net.wutnews.app.app.util.dbUtil;
+import net.wutnews.app.app.util.uurl;
+import net.wutnews.app.frame.IdoHttpUtil.HttpSender;
+import net.wutnews.app.frame.IdoHttpUtil.OnHttpResListener;
+import net.wutnews.app.frame.util.ImageLoaderUtil;
+import net.wutnews.app.frame.util.gsonUtil;
 
 public class UserCenter extends AppBaseAct implements View.OnClickListener {
 
     private TextView tv_name, tv_sign;
     private ImageView iv_head;
-
+    private GetUserDe uData;
+    private ImageLoaderUtil imageUtil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_center);
-
+        imageUtil = ImageLoaderUtil.getInstance();
         setTitleBar("个人中心", 0);
-
         findView();
-
+        getUserInfo();
     }
 
     private void findView() {
@@ -39,8 +44,9 @@ public class UserCenter extends AppBaseAct implements View.OnClickListener {
         iv_head.setOnClickListener(this);
         findViewById(R.id.ll_my_comment).setOnClickListener(this);
         findViewById(R.id.ll_my_collection).setOnClickListener(this);
+        findViewById(R.id.edt_profile).setOnClickListener(this);
         findViewById(R.id.ll_logout).setOnClickListener(this);
-        findViewById(R.id.mySignature).setOnClickListener(this);
+
     }
 
 
@@ -50,11 +56,8 @@ public class UserCenter extends AppBaseAct implements View.OnClickListener {
             default:
                 break;
             case R.id.iv_head:
-                break;
-            case R.id.mySignature:
-
-                Intent sigInte = new Intent(this, PostActivity.class);
-                startActivity(sigInte);
+                Intent hIntent=new Intent(this, AddPhotoActivity.class);
+                startActivity(hIntent);
                 break;
             case R.id.ll_my_comment:
 
@@ -66,6 +69,12 @@ public class UserCenter extends AppBaseAct implements View.OnClickListener {
                 Intent i = new Intent(this, MyCollection.class);
                 startActivity(i);
                 break;
+            case R.id.edt_profile:
+                Intent pIntent = new Intent(this, PostActivity.class);
+                pIntent.putExtra("nickname",uData.getUser_nicename());
+                pIntent.putExtra("signature",uData.getSignature());
+                startActivity(pIntent);
+                break;
             case R.id.ll_logout:
                 dbUtil.deleteTable(this, DBUserinfo.class);
                 MenuAct.mHandler.sendEmptyMessage(0);
@@ -73,5 +82,27 @@ public class UserCenter extends AppBaseAct implements View.OnClickListener {
                 finish();
                 break;
         }
+    }
+    private void getUserInfo()
+    {
+        SetUser set = new SetUser();
+        set.setUser(getUserinfo(this).getUser());
+        HttpSender sender = new HttpSender(uurl.GetUserInfo, "获取个人信息", set, new OnHttpResListener() {
+            @Override
+            public void doSuccess(String data) {
+                LogUtils.i("我的信息list: " + data.toString());
+                GetUserInfo temp = gsonUtil.getInstance().json2Bean(data, GetUserInfo.class);
+                if (temp.getMsg().equals("GET_SUCCESS")) {
+                    uData = temp.getData();
+                    tv_name.setText(uData.getUser_nicename());
+                    tv_sign.setText(uData.getSignature());
+                    if(uData.getAvatar()!=null)
+                    imageUtil.setImage(uurl.IMG_URL + uData.getAvatar(), iv_head);
+
+                }
+            }
+        });
+        sender.setContext(this);
+        sender.send(uurl.MODE);
     }
 }
